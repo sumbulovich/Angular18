b109cfa0e0e3ab4d67120123a1810ff56e6ffd1e
David <sumbulovich@gmail.com>
2024-10-24 09:20:52 +0200

    no message

--- src/app/features/dashboard/components/server-status/server-status.component.ts
+++ src/app/features/dashboard/components/server-status/server-status.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe, NgClass } from '@angular/common';
 import { Component, DestroyRef, OnInit, Signal, WritableSignal, inject, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { WebsocketService } from '@app/shared/services/websocket.service';
@@ -9,31 +10,36 @@ import { Observable, interval } from 'rxjs';
 @Component({
   selector: 'app-server-status',
   standalone: true,
-  imports: [NgClass, AsyncPipe, MatCardModule, MatIconModule],
+  imports: [NgClass, AsyncPipe, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.scss'
 })
 export class ServerStatusComponent {
   private destroyRef: DestroyRef = inject(DestroyRef)
   // Convert a Signal in to an Observable (it subscribes when signal changes)
-  status: WritableSignal<'online' | 'offline' | 'unknown'> = signal('offline');
-  status$: Observable<"online" | "offline" | "unknown"> = toObservable(this.status);
+  status: WritableSignal<'online' | 'offline' | 'paused'> = signal('paused');
+  status$: Observable<"online" | "offline" | "paused"> = toObservable(this.status);
   // Convert an Observable in to a Signal
   interval$: Observable<number> = interval(1000)
   interval: Signal<number> = toSignal(this.interval$, { initialValue: 0 });
   websocketService: WebsocketService = inject(WebsocketService);
+  isPaused: boolean = true;
 
   constructor() {
     // Server Side Render (SSR) needs call this method to start on the client side only
     // afterNextRender(() => {
     // setInterval(() => { ... })
     // });
-
-    this.interval = toSignal(this.websocketService.getMessages(), { initialValue: 0 });
-    const subscription = this.websocketService.getMessages().subscribe((number) => {
-      if (number > 50) this.status.set('online');
-      else if (number < 50) this.status.set('offline');
+    const subscription = this.websocketService.getMessages().subscribe((message) => {
+      this.status.set(message.status);
+      this.isPaused = message.status === 'paused';
     });
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  toggleStatus(): void {
+    const data = this.isPaused ? 'resume' : 'pause';
+    this.websocketService.sendMessage({ data });
+    this.isPaused = true;
   }
 }
